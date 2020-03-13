35707478597cf606e479e0f372921c8f727ffc59
k1211 <kgarbulewska@gmail.com>
2020-03-13 23:06:47 +0100

    Better item amout retrieving from row.

--- script.py
+++ script.py
@@ -5,6 +5,8 @@
 import argparse
 import os
 
+DAYS = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela']
+
 
 def handle_menu_file(menu_file):
     if os.path.isfile(menu_file):
@@ -24,33 +26,33 @@ def generate_shopping_list(menu_file_in_pdf):
     item_dict = {}
 
     for item in items:
-        abc = item.split(' x ')
+        item_info = item.split(' x ')
 
-        item_name = abc[0].split(' ')
-        the_item_name = f"{' '.join(item_name[:-1])}" if len(item_name) > 2 else item_name[0]
+        item_name_info = item_info[0].split(' ')
+        item_name = f"{' '.join(item_name_info[:-1])}" if len(item_name_info) > 2 else item_name_info[0]
 
-        if len(abc) == 1:
-            print(f"Undisclosed item amount for: {abc[0]}")
+        if len(item_info) == 1:
+            print(f"Undisclosed item amount for: {item_info[0]}")
             item_amount = 0
         else:
-            item_amount = abc[1].split(' ')
+            item_amount = item_info[1].split(' ')
             try:
-                item_amount = float(item_amount[1]) if len(item_amount) > 1 else 0
+                item_amount = float(item_amount[-2]) if len(item_amount) > 1 else 0
             except:
                 print(f"Unidentifiable item amount: {item_amount[1]}")
                 _item_amount = item_amount[1].replace('g', '')
                 item_amount = float(_item_amount) if len(item_amount) > 1 else 0
 
-        if the_item_name in item_dict:
-            item_dict[the_item_name] += item_amount
+        if item_name in item_dict:
+            item_dict[item_name] += item_amount
         else:
-            item_dict[the_item_name] = item_amount
+            item_dict[item_name] = item_amount
 
     sorted_dict = {k: item_dict[k] for k in sorted(item_dict)}
     return sorted_dict
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file_name", "-f", default="week.txt", help="File name with extension name.")
     args = parser.parse_args()
